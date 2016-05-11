3f6a89eacb00f0a4e1497485cd35e44c3bad96f5
administator <administator>
2016-05-11 16:11:58 +0800

    add check input

--- src/components/New.vue
+++ src/components/New.vue
@@ -2,42 +2,66 @@
 	<form class="form-horizontal">
 		<div class="form-group">
 			<label for="inputWord" class="col-sm-2 control-label">新词：</label>
-			<div class="col-sm-10">
-				<input type="text" class="form-control" id="inputWord" placeholder="new word" v-model="word.name">
+			<div class="col-sm-5">
+				<input type="text" class="form-control" id="inputWord" placeholder="new word" v-model="word.name.text" @blur="checkInput(word.name)">
 			</div>
+			<p class="col-sm-5 error-tip" v-if="!word.name.valid">
+				<i class="glyphicon glyphicon-info-sign"></i>
+				{{word.name.errorTip}}
+			</p>
 		</div>
 		<div class="form-group">
 			<label for="inputMean" class="col-sm-2 control-label">释义：</label>
-			<div class="col-sm-10">
-				<input type="text" class="form-control" id="inputMean" placeholder="mean" v-model="word.mean">
+			<div class="col-sm-5">
+				<textarea class="form-control" id="inputMean" placeholder="mean" v-model="word.mean.text" @blur="checkInput('mean')"></textarea>
 			</div>
+			<p class="col-sm-5 error-tip" v-if="!word.mean.valid">
+				<i class="glyphicon glyphicon-info-sign"></i>
+				{{word.mean.errorTip}}
+			</p>
 		</div>
 		<div class="form-group">
 			<label for="inputOrigin" class="col-sm-2 control-label">出处：</label>
-			<div class="col-sm-10">
-				<input type="text" class="form-control" id="inputOrigin" placeholder="origin" v-model="word.origin">
+			<div class="col-sm-5">
+				<input type="text" class="form-control" id="inputOrigin" placeholder="origin" v-model="word.origin.text">
 			</div>
+			<p class="col-sm-5 error-tip" v-if="!word.origin.valid">
+				<i class="glyphicon glyphicon-info-sign"></i>
+				{{word.origin.errorTip}}
+			</p>
 		</div>
 		<div class="form-group">
 			<label for="inputSame" class="col-sm-2 control-label">近义词：</label>
-			<div class="col-sm-10">
-				<input type="text" class="form-control" id="inputSame" placeholder="same" v-model="word.same">
+			<div class="col-sm-5">
+				<input type="text" class="form-control" id="inputSame" placeholder="same" v-model="word.same.text">
 			</div>
+			<p class="col-sm-5 error-tip" v-if="!word.same.valid">
+				<i class="glyphicon glyphicon-info-sign"></i>
+				{{word.same.errorTip}}
+			</p>
 		</div>
 		<div class="form-group">
 			<label for="inputLabel" class="col-sm-2 control-label">标签：</label>
-			<div class="col-sm-10">
-				<input type="text" class="form-control" id="inputLabel" placeholder="origin" v-model="word.label">
+			<div class="col-sm-5">
+				<input type="text" class="form-control" id="inputLabel" placeholder="origin" v-model="word.label.text">
 			</div>
+			<p class="col-sm-5 error-tip" v-if="!word.label.valid">
+				<i class="glyphicon glyphicon-info-sign"></i>
+				{{word.label.errorTip}}
+			</p>
 		</div>
 		<div class="form-group">
 			<label for="inputRate" class="col-sm-2 control-label">使用频率</label>
-			<div class="col-sm-10">
-				<input type="text" class="form-control" id="inputRate" placeholder="please enter 7 rates in a week split in ;" v-model="word.rate">
+			<div class="col-sm-5">
+				<input type="text" class="form-control" id="inputRate" placeholder="please enter 7 rates in a week split in ;" v-model="word.rate.text">
 			</div>
+			<p class="col-sm-5 error-tip" v-if="!word.rate.valid">
+				<i class="glyphicon glyphicon-info-sign"></i>
+				{{word.rate.errorTip}}
+			</p>
 		</div>
 		<div class="form-group">
-			<div class="col-sm-offset-2 col-sm-10">
+			<div class="col-sm-offset-2 col-sm-5">
 				<button type="button" class="btn btn-primary" @click="saveAdd(word)">add</button>
 			</div>
 		</div>
@@ -48,33 +72,33 @@
   export default {
     data () {
       var word = {
-        name: '',
-        mean: '',
-        origin: '',
-        same: '',
-        label: '',
-        rate: ''
+        name: {text: '', valid: true, errorTip: ''},
+        mean: {text: '', valid: true, errorTip: ''},
+        origin: {text: '', valid: true, errorTip: ''},
+        same: {text: '', valid: true, errorTip: ''},
+        label: {text: '', valid: true, errorTip: ''},
+        rate: {text: '', valid: true, errorTip: ''}
       }
       return {word: word}
     },
     methods: {
       saveAdd (word) {
-        console.log(word.same.split(' '))
-        console.log(word.label.split(' '))
-        console.log(word.rate.split(' '))
         var params = {
-          word: word.name,
-          mean: word.mean,
-          origin: word.origin,
-          same: word.same.split(' '),
-          label: word.label.split(' '),
-          rate: word.rate.split(' ')
+          word: word.name.text,
+          mean: word.mean.text,
+          origin: word.origin.text,
+          same: word.same.text.split(' '),
+          label: word.label.text.split(' '),
+          rate: word.rate.text.split(' ')
         }
         this.$http({url: '/saveWord', params, method: 'post'}).then(function (response) {
           console.log(response)
         }, function (response) {
           console.log(response)
         })
+      },
+      checkInput (params) {
+        console.log(params)
       }
     }
   }
